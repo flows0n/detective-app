f78491f7e76995d1776ebefc4b42fe6739b7546d
Bartek Maliszewski <bartosz.maliszewski@softmaker.io>
2023-04-21 14:13:04 +0200

    Removing unnesesary imports

--- src/views/ChooseView.tsx
+++ src/views/ChooseView.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
 import Button from "../components/Button";
 import Card from "../components/Card";
 import Header from "../components/Header";
--- src/views/SuccessView.tsx
+++ src/views/SuccessView.tsx
@@ -1,5 +1,5 @@
 import Card from "../components/Card";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "../components/Header";
 import styled from "styled-components";
 
